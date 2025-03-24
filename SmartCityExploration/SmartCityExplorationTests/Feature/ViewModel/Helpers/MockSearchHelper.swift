3aac6c2348a4dafdcf37afa62aa1fdcd0e256beb
Luis Segovia <luissegovia@outlook.com>
2025-03-24 13:23:52 -0600

    chore: move helpers to folder

---
+++ SmartCityExploration/SmartCityExplorationTests/Feature/ViewModel/Helpers/MockSearchHelper.swift
@@ -0,0 +1,19 @@
+//
+//  MockSearchHelper.swift
+//  SmartCityExploration
+//
+//  Created by Luis Segovia on 24/03/25.
+//
+
+@testable import SmartCityExploration
+
+final class MockSearchHelper: SearchHelperProtocol {
+    func initiate(with items: [SmartCityExploration.CityItem]) {
+        //
+    }
+    
+    func search(text: String) {
+        //
+    }
+
+}
