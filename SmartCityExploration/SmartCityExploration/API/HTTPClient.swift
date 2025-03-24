64ac09f28aeba40fc0306663ca9d421618a080f7
Luis Segovia <luissegovia@outlook.com>
2025-03-23 21:55:32 -0600

    feat: create `URLSessionHTTPClient` so it can be injected into API Clients

--- SmartCityExploration/SmartCityExploration/API/HTTPClient.swift
+++ SmartCityExploration/SmartCityExploration/API/HTTPClient.swift
@@ -12,3 +12,26 @@ protocol HTTPClient {
 
     func get(from url: URL) async -> Result
 }
+
+final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+
+    public init(session: URLSession) {
+        self.session = session
+    }
+
+    private struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL) async -> HTTPClient.Result {
+        do {
+            let (data, response) = try await session.data(from: url)
+            if let response = response as? HTTPURLResponse {
+                return .success((data, response))
+            } else {
+                throw UnexpectedValuesRepresentation()
+            }
+        } catch {
+            return .failure(NSError(domain: "", code: 0))
+        }
+    }
+}
