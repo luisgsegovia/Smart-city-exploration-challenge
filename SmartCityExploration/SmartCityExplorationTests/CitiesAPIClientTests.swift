620d36b2e775d424c1bf5c4530edcc2c3c6bd432
Luis Segovia <luissegovia@outlook.com>
2025-03-23 21:39:24 -0600

    test: retrieval of items returns decoded items successfully

--- SmartCityExploration/SmartCityExplorationTests/CitiesAPIClientTests.swift
+++ SmartCityExploration/SmartCityExplorationTests/CitiesAPIClientTests.swift
@@ -96,6 +96,16 @@ final class CitiesAPIClientTests: XCTestCase {
         XCTAssertEqual(result, .failure(.decodingError))
     }
 
+    func test_retrieveCities_returnsDecodedItems() async {
+        let (httpClient, sut) = makeSUT()
+        httpClient.clientResponse = .success((createValidJSONData(), .init()))
+        let expectedDecodedItem = CityResponseDTO(country: "MX", name: "Mexico City", id: 1234, coord: .init(lon: -10.00, lat: 10.00))
+
+        let result = await sut.retrieveCities()
+
+        XCTAssertEqual(result, .success([expectedDecodedItem]))
+    }
+
     private func makeSUT() -> (httpClient: HTTPClientMock, sut: CitiesAPIClient) {
         let httpClientMock = HTTPClientMock()
         let sut = CitiesAPIClient(httpClient: httpClientMock, url: anyURL())
@@ -115,6 +125,12 @@ final class CitiesAPIClientTests: XCTestCase {
         let invalidJSONData = ""
         return Data(invalidJSONData.utf8)
     }
+
+    private func createValidJSONData() -> Data {
+        let jsonData: String = "[{\"country\":\"MX\",\"name\":\"Mexico City\",\"_id\":1234,\"coord\":{\"lon\":-10.00,\"lat\":10.00}}]"
+        return Data(jsonData.utf8)
+    }
+
 }
 
 private final class HTTPClientMock: HTTPClient {
